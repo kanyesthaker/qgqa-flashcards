c5e75dcf2ebe6cf6437f74af3b055be95e23612f
samjgorman <samjgorman@gmail.com>
2021-07-26 15:20:29 -0700

    axios endpoint ready for prod

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,40 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
 import "./App.css";
 import Form from "./components/form.js";
 import FlashcardContainer from "./flashcard-container";
-
 // import Background from "./images/space.png";
-import styled, { css } from "styled-components";
-import { useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 //Let ESLint know that we are accessing Chrome browser methods
 /* global chrome */
 
-// const Bg = styled.div`
-//   backgroundimage: url(${Background});
-// `;
-
 function App() {
   const [url, setUrl] = useState("");
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    //Insert Chrome logic here to access current URL. Note that this runs on rerender
+    //Insert Chrome logic here to access current URL. Note that this runs on rerender,
+    // and breaks during development, so only uncomment for prod
     //https://blog.usejournal.com/making-an-interactive-chrome-extension-with-react-524483d7aa5d
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const url = new URL(tabs[0].url);
-      // const domain = url.hostname;
-      setUrl(url);
-    });
+    // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //   const tab_url = tabs[0].url;
+    //   setUrl(tab_url);
+    // });
+    //Now, send an Axios POST request
+    // const endpoint_string = "";
+    // axios
+    //   .post(endpoint_string, {
+    //     url: { url },
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //     setData(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
   });
   return (
     <div className="App">
-      {/* <div className="Bg" style={{ backgroundImage: `url(${Background})` }}> */}
       <header className="App-header">
         <Form></Form>
       </header>
       <FlashcardContainer></FlashcardContainer>
       <div>This is URL {url}</div>
-      {/* </div> */}
     </div>
   );
 }
