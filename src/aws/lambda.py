b90ff3b79418af3aa72510b1ef53c380c4a1d82c
kanyesthaker <kanyes.thaker@gmail.com>
2021-08-03 16:11:51 -0700

    update AWS Lambda function generate

--- src/aws/lambda.py
+++ src/aws/lambda.py
@@ -1,8 +1,10 @@
 import boto3
 import json
 from lambda_flashcards import LambdaFlashcards
+import urllib
 
 def generate(event, context):
 	src = event["queryStringParameters"]["src"]
+	src = urllib.parse.unquote(src).split("=")[-1]
 	flashcards = LambdaFlashcards()
 	return flashcards(src)
--- src/test.py
+++ src/test.py
@@ -1,32 +1,9 @@
-import os
-import io
-import boto3
-import json
-import csv
-import time
-import sagemaker
-from sagemaker.predictor import json_serializer, json_deserializer, Predictor
-
-ENDPOINT_NAME = "huggingface-pytorch-inference-2021-08-01-00-56-34-532"
-QG_NAME = "huggingface-pytorch-inference-2021-08-01-02-08-36-571"
+import requests
+import urllib
 
 if __name__=="__main__":
-    now = time.time()
-    payload = {"inputs": {
-                "question": "What is used for inference?",
-                "context": "My Name is Philipp and I live in Nuremberg. This model is used with sagemaker for inference."
-                }
-            }
-
-    predictor = Predictor(endpoint_name=ENDPOINT_NAME, sagemaker_session=sagemaker.Session(), serializer = json_serializer, content_type="text/csv", accept='application/json')
-    print(predictor.predict(payload))
-
-    qg_gen = Predictor(endpoint_name=ENDPOINT_NAME, sagemaker_session=sagemaker.Session(), seralizer=json_serializer, content_type="text/csv", accept='application/json')
-
-    args = {
-            "max_length": 2048, #256
-            "num_beams": 4,
-            "length_penalty": 0, #1.5
-            "no_repeat_ngram_size": 3,
-            "early_stopping": False, #True
-        }
+    src = "https://en.wikipedia.org/wiki/Ghoul"
+    params = {"src":src}
+    encoded = urllib.parse.urlencode(params)
+    api=f"https://cbczedlkid.execute-api.us-west-2.amazonaws.com/test/generate?{encoded}"
+    print(requests.get(api))
