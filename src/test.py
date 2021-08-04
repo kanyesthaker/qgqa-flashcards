41659910cebcd4d186b54cf15a5b321782d6b786
kanyesthaker <kanyes.thaker@gmail.com>
2021-08-03 22:15:42 -0700

    endpoint working

--- src/test.py
+++ src/test.py
@@ -1,9 +1,11 @@
 import requests
 import urllib
+import json
 
 if __name__=="__main__":
     src = "https://en.wikipedia.org/wiki/Ghoul"
     params = {"src":src}
     encoded = urllib.parse.urlencode(params)
     api=f"https://cbczedlkid.execute-api.us-west-2.amazonaws.com/test/generate?{encoded}"
-    print(requests.get(api))
+    print(api)
+    print(json.dumps(requests.get(api).json()))
