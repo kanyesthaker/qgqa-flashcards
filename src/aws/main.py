619a3e5370257b1faa38be64ab789de3480734b4
kanyesthaker <kanyes.thaker@gmail.com>
2021-08-04 12:00:36 -0700

    update AWS Lambda function segment-text

--- src/aws/main.py
+++ src/aws/main.py
@@ -6,50 +6,50 @@ import uuid
 from scraper import Scraper
 
 def generate(event, context):
-	src = event["queryStringParameters"]["src"]
-	src = urllib.parse.unquote(src).split("=")[-1]
-	print(f"FUNCTION generate IN main.py CALLED WITH ARGUMENT {src}")
-	flashcards = LambdaFlashcards()
-	print("LambdaFlashcards OBJECT INSTANTIATED")
-	questions = flashcards(src)
-	r = {str(uuid.uuid4()) : questions}
-	response = {
-		"isBase64Encoded":False,
+    src = event["queryStringParameters"]["src"]
+    src = urllib.parse.unquote(src).split("=")[-1]
+    print(f"FUNCTION generate IN main.py CALLED WITH ARGUMENT {src}")
+    flashcards = LambdaFlashcards()
+    print("LambdaFlashcards OBJECT INSTANTIATED")
+    questions = flashcards(src)
+    r = {str(uuid.uuid4()) : questions}
+    response = {
+        "isBase64Encoded":False,
         "statusCode": 200,
         "headers":{
-        	"Content-Type":"application/json",
-        	"Access-Control-Allow-Origin":"*",
+            "Content-Type":"application/json",
+            "Access-Control-Allow-Origin":"*",
         },
         "body": json.dumps(r)
     }
 
-	print(r)
-	return response
+    print(r)
+    return response
 
 def segment_text(event, context):
-	batch_size=50
-	src = event["queryStringParameters"]["src"]
-	src = urllib.parse.unquote(src).split("=")[-1]
-	scraper = Scraper()
-	context=scraper.get_text(src)
-	tokens = context.split()
+    batch_size=50
+    src = event["queryStringParameters"]["src"]
+    src = urllib.parse.unquote(src).split("=")[-1]
+    scraper = Scraper()
+    context=scraper.get_text(src)
+    tokens = context.split()
     question_batches = [' '.join(tokens[i:i+batch_size]) for i in range(0, len(tokens), batch_size)]
     ans_batches = {}
     for i, e in enumerate(question_batches):
-    	if i == 0:
-    		ans_batches[i] = question_batches[i:i+2]
-    	else:
-    		ans_batches[i] = question_batches[i-1:i+2]
+        if i == 0:
+            ans_batches[i] = question_batches[i:i+2]
+        else:
+            ans_batches[i] = question_batches[i-1:i+2]
 
     r = {str(uuid.uuid4()): ans_batches}
     response = {
-    	"isBase64Encoded":False,
-    	"statusCode":200,
-    	"headers":{
-    		"Content-Type":"application/json",
-    		"Access-Control-Allow-Origin":"*",
-    	},
-    	"body":json.dumps(r)
+        "isBase64Encoded":False,
+        "statusCode":200,
+        "headers":{
+            "Content-Type":"application/json",
+            "Access-Control-Allow-Origin":"*",
+        },
+        "body":json.dumps(r)
     }
 
     print(r)
--- src/test.py
+++ src/test.py
@@ -6,6 +6,6 @@ if __name__=="__main__":
     src = "https://en.wikipedia.org/wiki/Ghoul"
     params = {"src":src}
     encoded = urllib.parse.urlencode(params)
-    api=f"https://cbczedlkid.execute-api.us-west-2.amazonaws.com/test/generate?{encoded}"
+    api=f"https://cbczedlkid.execute-api.us-west-2.amazonaws.com/ferret-alpha/segment-text?{encoded}"
     print(api)
     print(json.dumps(requests.get(api).json()))
