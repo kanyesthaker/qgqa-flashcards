9811fd81037daa435988bd41508b5681b887e485
kanyesthaker <kanyes.thaker@gmail.com>
2021-08-03 17:30:56 -0700

    deploy AWS Lambda function generate

--- src/aws/deploy.sh
+++ src/aws/deploy.sh
@@ -37,6 +37,11 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 pip install --target ./package -r requirements.txt
+find ./package -type d -name "tests" -exec rm -rfv {} +
+find ./package -type d -name "__pycache__" -exec rm -rfv {} +
+rm -rf package/numpy*
+curl "https://files.pythonhosted.org/packages/9b/04/c3846024ddc7514cde17087f62f0502abf85c53e8f69f6312c70db6d144e/numpy-1.19.2-cp37-cp37m-manylinux2010_x86_64.whl" -o "numpy-1.19.2-cp36-cp36m-manylinux1_x86_64.whl"
+unzip numpy-1.19.2-cp37-cp37m-manylinux2010_x86_64.whl -d package
 cd package
 zip -r9 ${OLDPWD}/function.zip .
 cd ${OLDPWD}
